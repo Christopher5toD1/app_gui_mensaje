04d7ab11f34146ebbf8e2e6c01ae57b37e92f26b
PC <PC@DESKTOP-HLQM19M>
2024-02-11 23:11:35 -0400

    Agregar archivos de proyecto.

---
+++ app_gui_mensaje/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace app_gui_mensaje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.BtnMostral = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSalir.Location = new System.Drawing.Point(351, 66);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(184, 45);
+            this.BtnSalir.TabIndex = 0;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // BtnMostral
+            // 
+            this.BtnMostral.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMostral.Location = new System.Drawing.Point(351, 148);
+            this.BtnMostral.Name = "BtnMostral";
+            this.BtnMostral.Size = new System.Drawing.Size(184, 45);
+            this.BtnMostral.TabIndex = 1;
+            this.BtnMostral.Text = "Mostral mensaje";
+            this.BtnMostral.UseVisualStyleBackColor = true;
+            this.BtnMostral.Click += new System.EventHandler(this.BtnMostral_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 305);
+            this.Controls.Add(this.BtnMostral);
+            this.Controls.Add(this.BtnSalir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Button BtnMostral;
+    }
+}
+
---
+++ app_gui_mensaje/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app_gui_mensaje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMostral_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Eso no es de Ganster");
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult Resultado;
+
+            Resultado = MessageBox.Show("Seguro que deseas salir","",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+          
+            if (Resultado == DialogResult.Yes)
+
+            { Close(); }
+        }
+    }
+}
